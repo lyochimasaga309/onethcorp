68b3398898ec4d99c40703c6869c9803ddc7b3ba
oneth-corp <onethcorporation@gmail.com>
2025-08-06 23:06:39 -0700

    Fix: corrected route file path for Render

--- backend/routes/HouseRoutes.js
+++ backend/routes/HouseRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const houseRoutes = require("./routes/houseRoutes");
+const houseRoutes = require("../routes/houseRoutes");
 
 // GET all houses
 router.get("/", async (req, res) => {
